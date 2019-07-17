5e01d2f003b59f3c646f71464e9ebd0a7e0a2a8f
Sean Harding <shadowbomb20@gmail.com>
2019-07-17 13:37:11 -1000

    Renamed for standards and changed details

--- lib/dtg.rb
+++ lib/dtg.rb
@@ -2,7 +2,7 @@ require "dtg/railtie"
 require "dtg/zones"
 require "dtg/core_datetime_ext"
 require "dtg/core_time_ext"
-require "dtg/core_time_with_zone_ext"
+require "dtg/core_timewithzone_ext"
 
 module Dtg
   def dtg
